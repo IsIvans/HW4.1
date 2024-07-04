c52a57d6236d3d3c1c247d7f44f6bf57d8be8c7c
IsIvans <is.ivanchenko@gmail.com>
2024-07-04 22:17:13 +0300

    HW6.1

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,21 +1,19 @@
 package app;
 
 public class Main {
-
-    static double finance;
-    static double taxes;
-    static double taxRate;
-
+    static int sum;
+    static int number;
 
     public static void main(String[] args) {
 
-        finance = 11456;
-        taxRate = finance > 0 && finance <= 10000 ? 2.5 : finance > 10000 && finance <= 25000 ? 4.3 : 6.7;
-        if (finance < 0) {
-            System.out.println("Error");
-        }else{
-            taxes = (finance * taxRate) / 100;
-            System.out.printf("Tax amount = "+"%.2f",taxes);
+        for (int i = 1; i<=6; i++) {
+            sum += i;
+            number++;
+            System.out.print(number+") Num is "+ i+", ");
+            System.out.println("sum is " + sum);
         }
+        System.out.println("------------------------");
+        System.out.println("Sum of numbers is "+ sum);
+
     }
 }
